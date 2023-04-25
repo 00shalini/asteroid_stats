b70b13ea3e646ef24621dc532d14e25c46e6af9d
00Shalini <shalini07061997@gmail.com>
2023-04-25 22:33:51 +0530

    get asteroid stats

--- src/Component/AsteroidHome.jsx
+++ src/Component/AsteroidHome.jsx
@@ -1,30 +1,40 @@
-import {React, useState} from 'react';
+import {React, useEffect, useState} from 'react';
 import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 const AsteroidHome = () => {
 
     const [ startDate, setStartDate] = useState('');
     const [endDate, setEndDate] = useState('');
+    const [data, setData] = useState([]);
+    const API_KEY = 'flnlJxR2uZfnCMtnIiFTtcC1WfvbDlIqbk57aBbU';
+    const handleStartDate = (e) => {
+      const startD = e.target.value;
+      setStartDate(startD);
+    }
 
     const handleEndDate = (e) => {
-      const startD = e.target.value;
-      console.log(startD)
-    }
-
-    const handleStartDate = (e) => {
       const endD =  e.target.value;
-      console.log(endD)
+      setEndDate(endD)
     }
 
-    
-    
+    const getAsteroidStats = async () => {
+        try {
+            const response = await fetch(`https://api.nasa.gov/neo/rest/v1/feed?start_date=${startDate}&end_date=${endDate}&api_key=${API_KEY}`); // Replace with your API endpoint
+            const data = await response.json();
+            console.log(data)
+            //setData(data); // Set the fetched data in state
+          } catch (error) {
+            console.error('Error fetching data:', error);
+          }
+    }
+
     return (
        <div className='container'>
         <h2 className='text-center'>Welcome Earthlings</h2>
         <div className='d-flex justify-content-around'>
-           <div className='container'><span>Start Date</span> <input className='w-25 h-50' type='date' value={startDate} onChange={(e) => handleStartDate(e)}/></div>
-           <div className='container'><span>End Date</span> <input className='w-25 h-50' type='date' value={endDate} onChange={(e) => handleEndDate(e)}/></div>
-            <button type='button' className='btn btn-success w-25 h-50'>Submit</button>
+           <div><span>Start Date</span> <input className='w-25 h-50' type='date' value={startDate} name='startdate' onChange={(e) => handleStartDate(e)}/></div>
+           <div><span>End Date</span> <input className='w-25 h-50' type='date' value={endDate} name='enddate' onChange={(e) => handleEndDate(e)}/></div>
+            <button type='button' className='btn btn-success w-25 h-50' onClick={() => getAsteroidStats()}>Submit</button>
         </div>
        </div>
     )
